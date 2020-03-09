c23007b700a3899ae93842646530a825802d4776
Webserver User <u607664@m39s4-1-2db.ispgateway.de>
2020-03-09 18:02:18 +0100

    composer update

--- start.php
+++ start.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/MonologToLoggly.php");
 
 
 $today = date("Y-m-d");
-//$today = "2018-10-09";
+# $today = "2020-03-08";
 $logfilename = $today . '.log';
 
 $L = new MonologToLoggly();
