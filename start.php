9a37a6855b38b86c19416e7cc28b9f8d086ed9c6
DomainFactory <support@planet-itservices.com>
2020-03-09 17:40:23 +0100

    monolog 2 loggly update

--- start.php
+++ start.php
@@ -4,5 +4,12 @@ require_once(__DIR__ . "/vendor/autoload.php");
 require_once(__DIR__ . "/MonologToLoggly.php");
 
 
+$today = date("Y-m-d");
+//$today = "2018-10-09";
+$logfilename = $today . '.log';
+
 $L = new MonologToLoggly();
-$L->uploadLogFile("logs/2018-10-06.log");
+
+
+$L->uploadLogFile("../logs/" . $logfilename);
+
